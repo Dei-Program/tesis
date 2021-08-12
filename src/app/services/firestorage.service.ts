5d8b0ee8a3c0adcd5b23701454d39d4bb940d4c2
Dei-Program <david.condor@epn.edu.ec>
2021-08-11 20:13:54 -0500

    CHAT and ubication time real 2

---
+++ src/app/services/firestorage.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {finalize} from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FirestorageService {
+
+    constructor(private storage: AngularFireStorage) {
+    }
+    // casi
+    // uploadImage() {
+    //     return new Promise(resolve => {
+    //         setTimeout(() => {
+    //             resolve(true);
+    //             console.log('responde  la promesa');
+    //             return;
+    //         }, 2000);
+    //
+    //     });
+    // }
+    uploadImage(file: any, path: string, nombre: string): Promise<string> {
+        return new Promise(resolve => {
+            const filePath = path + '/' + nombre;
+            const ref = this.storage.ref(filePath);
+            const task = ref.put(file);
+            task.snapshotChanges().pipe(
+                finalize(() => {
+                    ref.getDownloadURL().subscribe( res => {
+                        const downloadURL = res;
+                        resolve(downloadURL);
+                        return;
+                        }
+                    );
+                })
+            ).subscribe();
+        });
+    }
+}
